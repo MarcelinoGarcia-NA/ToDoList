bd899b19a5cfbca7dc9a47e51f286cdc50b401b0
MarcelinoGarcia-NA <64509713+MarcelinoGarcia-NA@users.noreply.github.com>
2023-06-26 21:16:00 -0300

    create component TodoForm: In home function delete e alter

--- src/app/components/todo.tsx
+++ src/app/components/todo.tsx
@@ -1,17 +1,24 @@
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import todo from "../types/todo";
 
+export default function Todo({ todo, remove, alter }: Params) {
 
-export default function Todo({ props }: Params) {
-
+    const handlerRemove = (id:number) => {
+        remove(id);
+    }
+    const handlerAlter = (id:number) => {
+        alter(id);
+    }
+    
     return (
         <div>
-            {props.map((todo:Params)=>(
+            {todo.map((todo: todo) => (
                 <div key={todo.id}>
-                    <p>{todo.text}</p>
-                    <button>CONCLUIDA</button>
-                    <button>X</button>
+                    <p style={{ textDecoration: todo.isActive ? "line-through" : "" }}>{todo.text}</p>
+                    <button onClick={()=>handlerAlter(todo.id)}>CONCLUIDA</button>
+                    <button onClick={()=>handlerRemove(todo.id)}>X</button>
                 </div>
-                ))}
+            ))}
         </div>
     );
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import todo from "./types/todo"
 import { useState } from "react";
 
 export default function Home() {
-  const [toDoList,setToDoList] = useState([
+  const [toDoList, setToDoList] = useState([
     {
       id: 1,
       text: 'Faxina em casa!',
@@ -19,21 +19,31 @@ export default function Home() {
     }
   ]);
 
-  function create(text:string){
-    const newtoDoList:todo[]=[...toDoList,{
-             id:Math.floor(Math.random() * 1000),
-             text:text,
-             isActive:true,
-          },
+  function create(text: string) {
+    const newtoDoList: todo[] = [...toDoList, {
+      id: Math.floor(Math.random() * 1000),
+      text: text,
+      isActive: true,
+    },
     ]
-     setToDoList(newtoDoList);
+    setToDoList(newtoDoList);
+  }
+  function remove(id: number) {
+    const newtoDolist: todo[] = [...toDoList];
+    const list = newtoDolist.filter(todo => (todo.id !== id ? todo : null));
+    setToDoList(list);
+  }
+  function alter(id:number){
+    const newtoDolist: todo[] = [...toDoList];
+    newtoDolist.map(todo => (todo.id === id ? todo.isActive = !todo.isActive:todo ));
+    setToDoList(newtoDolist);
   }
 
   return (
     <main>
       <h1>Lista de a fazeres</h1>
-      <TodoForm addTodo={create}/>
-      <Todo props={toDoList}/>
+      <TodoForm addTodo={create} />
+      <Todo todo={toDoList} remove={remove} alter={alter} />
     </main>
   )
 }
